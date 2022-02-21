da1d200496a633cb5f702d2345d76dbca39784d9
SubhamMandal <iamsubhammandal.sm@gmail.com>
2022-02-21 15:33:09 +0530

    changing content

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# My-Website
+# Buddhu
+Buddhu's personal website
--- script.js
+++ script.js
@@ -1,73 +1,110 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+// ============================ Hide Menu on navigation =================== 
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
-
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
-
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
+document.querySelectorAll(".nav-buttons").forEach(item => {
+    item.addEventListener('click', () => {
+        document.getElementById("click").checked = false;
+    })
 });
+
+//============================= Typing Effect============================================== 
+
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
+
+var i = 0, j = 0;
+var txt = [ 'SOFTWARE ENGINEER', 'COMPETITIVE CODER', 'WEB DEVELOPER', 'DESIGNER']; 
+var speed = 50; 
+async function typeWriter() {
+    if (i < txt[j].length) {
+        document.getElementById("typing").innerHTML += txt[j].charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    } else if (j < txt.length - 1) {
+        await wait(3000);
+        j++;
+        i = 0;
+        document.getElementById("typing").innerHTML = "";
+        setTimeout(typeWriter, speed);
+    } else {
+        await wait(5000);
+        document.getElementById("typing").innerHTML = "";
+        j = 0;
+        i = 0;
+        setTimeout(typeWriter, speed);
+    }
+}
+setTimeout(typeWriter, 0);
+
+// ============================ Image Slider Effect =======================================
+
+const slides = document.getElementsByClassName('slide');
+let counter = 1;
+
+function slide(n) {
+    let i;
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"
+    }
+    if (n < 1) counter = slides.length;
+    if (n > slides.length) counter = 1;
+    slides[counter - 1].style.display = "block";
+}
+
+let timer = setInterval(autoSlide, 10000)
+function autoSlide(){
+    counter++;
+    slide(counter);
+}
+
+// ====================== Project display slider ===========================
+
+
+const project1 = document.getElementsByClassName('a');
+const project2 = document.getElementsByClassName('b');
+const project3 = document.getElementsByClassName('c');
+
+let currentImg1 = 0;
+let currentImg2 = 0;
+let currentImg3 = 0;
+
+
+function projectSlider(projectImages, currentImg){
+    let i;
+    for(i=0;i<projectImages.length;i++){
+        projectImages[i].style.display = "none";
+    }
+    projectImages[currentImg].style.display = "block";
+}
+
+setInterval(() => {
+    projectSlider(project1, currentImg1);
+    currentImg1++;
+    if(currentImg1 > 2) currentImg1 = 0;
+},8000);
+
+setTimeout(()=>{
+    setInterval(() => {
+        projectSlider(project2, currentImg2);
+        currentImg2++;
+        if(currentImg2 > 2) currentImg2 = 0;
+    },8000);
+}, 3000);
+
+setTimeout(()=>{
+    setInterval(() => {
+        projectSlider(project3, currentImg3);
+        currentImg3++;
+        if(currentImg3 > 2) currentImg3 = 0;
+    },8000);
+}, 5000);
+
+// ================================ Dark Mode ==============================
+
+const darkModeIcon = document.querySelector('.dark-mode-icon');
+
+darkModeIcon.addEventListener('click', flipDark);
+
+function flipDark() {
+    document.body.classList.toggle('dark-mode');
+}
